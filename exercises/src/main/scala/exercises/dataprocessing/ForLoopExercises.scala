82a0b82ae6aadeee6eedd0df4e2ce5dcd27f1eaa
Aaron Mast <amast09@gmail.com>
2023-02-07 12:50:55 -0500

    complete for loops section

--- exercises/src/main/scala/exercises/dataprocessing/ForLoopExercises.scala
+++ exercises/src/main/scala/exercises/dataprocessing/ForLoopExercises.scala
@@ -2,20 +2,14 @@ package exercises.dataprocessing
 
 object ForLoopExercises {
 
-  def sum(numbers: List[Int]): Int = {
-    var total = 0
-
-    for (number <- numbers)
-      total += number
-
-    total
-  }
+  def sum(numbers: List[Int]): Int =
+    foldLeft[Int, Int](0)(_ + _)(numbers)
 
   // a. Implement `size` using a mutable state and a for loop
   // such as size(List(2,5,1,8)) == 4
   // and     size(Nil) == 0
   def size[A](items: List[A]): Int =
-    ???
+    foldLeft[Int, A](0)((acc, _) => acc + 1)(items)
 
   // b. Implement `min` using a mutable state and a for loop
   // such as min(List(2,5,1,8)) == Some(1)
@@ -24,7 +18,7 @@ object ForLoopExercises {
   // * Some when there is a value and
   // * None when there is no value (a bit like null)
   def min(numbers: List[Int]): Option[Int] =
-    ???
+    generalMin(numbers)
 
   // c. Implement `wordCount` using a mutable state and a for loop.
   // `wordCount` compute how many times each word appears in a `List`
@@ -33,12 +27,22 @@ object ForLoopExercises {
   // Note: You can lookup an element in a `Map` with the method `get`
   // and you can upsert a value using `updated`
   def wordCount(words: List[String]): Map[String, Int] =
-    ???
+    foldLeft[Map[String, Int], String](Map.empty) { (wordCounts, word) =>
+      val currentCountForWord = wordCounts.get(word).getOrElse(0)
+      wordCounts + (word -> (currentCountForWord + 1))
+    }(words)
 
   // d. `sum`, `size`, `min` and `wordCount` are quite similar.
   // Could you write a higher-order function that captures this pattern?
   // How would you call it?
-  def pattern = ???
+  def foldLeft[Acc, Elem](initialAccValue: Acc)(accFunction: (Acc, Elem) => Acc)(elements: List[Elem]): Acc = {
+    var finalAccValue = initialAccValue
+
+    for (element <- elements)
+      finalAccValue = accFunction(finalAccValue, element)
+
+    finalAccValue
+  }
 
   // e. Refactor `sum`, `size`, `min` and `wordCount` using the higher-order
   // function you defined above.
@@ -51,21 +55,25 @@ object ForLoopExercises {
   // Do you want to give it a try? For example, can you implement
   // `map`, `reverse` and `lastOption` in terms of `foldLeft`
   def map[From, To](elements: List[From])(update: From => To): List[To] =
-    ???
+    foldLeft[List[To], From](List())(_ :+ update(_))(elements)
 
   // reverse(List(3,8,1)) == List(1,8,3)
   // reverse(Nil) == Nil
   def reverse[A](elements: List[A]): List[A] =
-    ???
+    foldLeft[List[A], A](List())((xs, s) => s +: xs)(elements)
 
   // lastOption(List(3,8,1)) == Some(1)
   // lastOption(Nil) == None
   def lastOption[A](elements: List[A]): Option[A] =
-    ???
+    foldLeft[Option[A], A](None)((acc, el) => Some(el))(elements)
 
-  // g. Can you generalise `min` so that it applies to more types like `Long`, `String`, ...?
+  // g. Can you generalize `min` so that it applies to more types like `Long`, `String`, ...?
   // Note: You may want to use the class Ordering from the standard library
-  def generalMin[A](elements: List[A]): Option[A] =
-    ???
+  def generalMin[A](elements: List[A])(implicit ord: math.Ordering[A]): Option[A] =
+    foldLeft[Option[A], A](None) {
+      case (Some(value), elem) if ord.compare(value, elem) < 0 =>
+        Some(value)
+      case (_, elem) => Some(elem)
+    }(elements)
 
 }
--- exercises/src/test/scala/exercises/dataprocessing/ForLoopExercisesTest.scala
+++ exercises/src/test/scala/exercises/dataprocessing/ForLoopExercisesTest.scala
@@ -17,19 +17,115 @@ class ForLoopExercisesTest extends AnyFunSuite with ScalaCheckDrivenPropertyChec
     }
   }
 
-  ignore("size") {
+  test("size") {
     assert(size(List(2, 5, 1, 8)) == 4)
     assert(size(Nil) == 0)
   }
 
-  ignore("min") {
-    assert(min(List(2, 5, 1, 8)) == Some(1))
-    assert(min(Nil) == None)
+  test("size is consistent with List size") {
+    forAll { (numbers: List[Int]) =>
+      assert(size(numbers) == numbers.size)
+    }
   }
 
-  ignore("wordCount") {
+  test("the size of 2 lists combined is consistent") {
+    forAll { (list1: List[Int], list2: List[Int]) =>
+      assert(size(list1) + size(list2) == size(list1 ++ list2))
+    }
+  }
+
+  test("min") {
+    assert(min(List(2, 5, 1, 8)) == Some(1))
+    assert(min(Nil) == None)
+    assert(min(List()) == None)
+  }
+
+  test("min returns the smallest value in the list") {
+    forAll { (list: List[Int]) =>
+      min(list) match {
+        case None            => assert(list.isEmpty)
+        case Some(minOfList) => assert(list.forall(minOfList <= _))
+      }
+    }
+  }
+
+  test("min returns an element contained in the list") {
+    forAll { (list: List[Int]) =>
+      assert(
+        min(list)
+          .map(minOfList => list.contains(minOfList))
+          .getOrElse(true)
+      )
+    }
+  }
+
+  test("wordCount") {
     assert(wordCount(List("Hi", "Hello", "Hi")) == Map("Hi" -> 2, "Hello" -> 1))
     assert(wordCount(Nil) == Map.empty)
   }
 
+  test("wordCount returns a map with all elements") {
+    forAll { (list: List[String]) =>
+      val wordCounts = wordCount(list)
+      list.foreach { e =>
+        assert(wordCounts.get(e).map(_ => true).getOrElse(false))
+      }
+    }
+  }
+
+  test("wordCount adds up to size of list") {
+    forAll { (list: List[String]) =>
+      val wordCounts = wordCount(list)
+      val totalCount = wordCounts.values.foldLeft(0)(_ + _)
+      assert(totalCount == list.length)
+    }
+  }
+
+  test("wordCount all frequencies are positive") {
+    forAll { (list: List[String]) =>
+      assert(wordCount(list).values.forall(_ > 0))
+    }
+  }
+
+  test("foldLeft processes elements left to right") {
+    forAll { (list: List[String]) =>
+      assert(foldLeft[List[String], String](List())(_ :+ _)(list) == list)
+    }
+  }
+
+  test("map") {
+    forAll { (list: List[String], mapFunc: String => String) =>
+      val mappedList = map(list)(mapFunc)
+
+      mappedList.zipWithIndex.foreach { case (mappedElem, idx) =>
+        assert(mappedElem == mapFunc(list(idx)))
+      }
+    }
+  }
+
+  test("reverse") {
+    forAll { (list: List[String], mapFunc: String => String) =>
+      val mappedList = reverse(list)
+
+      mappedList.zipWithIndex.foreach { case (mappedElem, idx) =>
+        assert(mappedElem == list((list.length - 1) - idx))
+      }
+    }
+  }
+
+  test("lastOption") {
+    forAll { (list: List[String]) =>
+      lastOption(list) match {
+        case None        => assert(list.isEmpty)
+        case Some(value) => assert(value == list(list.length - 1))
+      }
+    }
+  }
+
+  test("generalMin") {
+    assert(generalMin(List(2, 5, 1, 8)) == Some(1))
+    assert(generalMin(List('A', 'B', 'C')) == Some('A'))
+    assert(generalMin(List("Foo", "Bar", "Baz")) == Some("Bar"))
+    assert(generalMin[Int](List()) == None)
+  }
 }
